1e2484332a6e8aafce8e5ad8ea5ac07407d3de2f
nuwrldnf8r <nuwrldnf8r@gmail.com>
2021-10-18 23:35:23 +0200

    Update contractAddress.js
    Added forest factory

--- src/crypto/contractAddress.js
+++ src/crypto/contractAddress.js
@@ -4,7 +4,8 @@ const addresses = {
     lockFactorySeed: '0x4014525e13600dA6Ab2eb89ABEd2E12CdabEC6d0',
     lockFactoryTeam: '0x19DeC8d99a786f5F876a6bD17c91E3CF0c0f0306',
     elysFtmPair: '0x6831b2EDe25Dcc957256FAE815f051181F6C7b08',
-    ftmUsdPair: '0x2b4C76d0dc16BE1C31D4C1DC53bF9B45987Fc75c'
+    ftmUsdPair: '0x2b4C76d0dc16BE1C31D4C1DC53bF9B45987Fc75c',
+    forestFactory: '0x4000723Ce9354e9085783A6Cd533BD528327D23E'
 }
 
 export default addresses
