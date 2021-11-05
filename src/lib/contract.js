67c8c5d3c73cfb48f618d5870780892d52e1cf71
nuwrldnf8r <nuwrldnf8r@gmail.com>
2021-11-05 12:29:39 +0200

    made fixes to forest code

--- src/lib/contract.js
+++ src/lib/contract.js
@@ -3,7 +3,9 @@ import abi from '../crypto/abi';
 
 class Contract {
     _getAcc = async () => {
-        let accs = await this.w3.eth.getAccounts()
+        //let accs = await this.w3.eth.getAccounts()
+        await window.ethereum.enable()
+        const accs = await window.ethereum.request({ method: 'eth_requestAccounts' });
         let acc = accs[0];
         return acc;
     }
@@ -28,19 +30,26 @@ class Contract {
                 if(params===undefined) params=[];
                 if( params.length!==method.inputs.length) throw new Error("Incorrect number of parameters");
                 let func = await this._contract.methods[_method](...params);
-                let gas = 500000;//await func.estimateGas();
-                let gasPrice = await this.w3.eth.getGasPrice();
-                let options = {from:_acc, gas: gas, gasPrice: gasPrice}; //
+                let options = {from:_acc}; 
                 if(method.stateMutability==="payable"){
                     if(value===undefined || value===0) throw new Error("value needs to be set for payable methods");
                     options.value = value;
                 }
                 if(method.stateMutability==="view"){
-                    delete options.gas
-                    delete options.gasPrice
                     let res = await func.call(options);
                     return res;
                 } else {
+                    options.gasPrice = await this.w3.eth.getGasPrice();
+                    let gasEstOptions = {from: options.from, gasPrice: options.gasPrice}
+                    if(value>0) gasEstOptions.value = value
+                    try{
+                        options.gas =  await func.estimateGas(gasEstOptions)
+                    }
+                    catch(e){
+                        console.log(e)
+                        options.gas = 2000000
+                    }
+                    console.log(options)
                     let res = await func.send(options);
                     return res;
                 }
